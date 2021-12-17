74459d278160d07af0778384ac36150d1a3a5655
mark <markpackham1@gmail.com>
2021-12-17 13:01:07 +0000

    create square component
    https://youtu.be/G0bBLvWXBvc?t=427

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      square works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SquareComponent {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
